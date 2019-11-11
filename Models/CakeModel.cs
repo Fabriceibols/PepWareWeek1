b55d5162e2689925fe1ef60471d77deaccc88a79
Kibuluku <fabriceibols@gmail.com>
2019-11-11 20:29:42 +0200

    Add files via upload

---
+++ Models/CakeModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PepWareWeek1.Models
+{
+    public class CakeModel
+    {
+        public int NumSold { get; set; }
+        public int TotSalAmt { get; set; }
+        public int Delux { get; set; }
+
+    }
+}
